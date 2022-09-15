a69f78bb9208b2d1ce78bad581423f6526077c7f
Tomás F <15079464+tomasff@users.noreply.github.com>
2022-09-15 15:50:47 +0100

    Add routes for starting/playing games

--- coursework/quizz_app/game.py
+++ coursework/quizz_app/game.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -9,3 +10,11 @@ bp = Blueprint('game', __name__)
 @bp.route('/')
 def index():
     return render_template('home.html')
+
+@bp.route('/game/start')
+def start():
+    return redirect(url_for('game.play', id=uuid4()))
+
+@bp.route('/game/<string:id>/play')
+def play(id):
+    return f'Playing game {id}...'
