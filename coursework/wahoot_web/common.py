f19605330930bfc6f874bee58c504e2ddb59f83d
EdmundGoodman <egoodman3141@gmail.com>
2023-07-28 22:03:02 +0100

    Remove coursework content

--- coursework/wahoot_web/common.py
+++
@@ -1,7 +0,0 @@
-from wahoot.repositories import QuestionBank, GameHistory
-from wahoot.services import GameService
-
-# Retrieve stored game data and questions
-game_history = GameHistory()
-question_bank = QuestionBank("test.csv")
-game_service = GameService(game_history, question_bank)
